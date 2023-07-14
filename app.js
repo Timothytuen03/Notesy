4a7f2c7977f2e0a3d0ba222467cc570adc07c3a4
Timothy Tuen <timothytuen45@gmail.com>
2023-07-14 13:51:16 -0400

    Add database retrieval and updates/deletes to the note creation and deletion functionality

--- app.js
+++ app.js
@@ -10,20 +10,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 mongoose.connect('mongodb://localhost:27017/NotesyDB', { useNewUrlParser: true });
 
+// const categorySchema
+
 const userSchema = new mongoose.Schema({
-    // userID: {
-    //     type: Number,
-    //     required: true
-    // },
     email: {
-        type: email,
+        type: String,
         required: true
     },
     password: {
         type: String,
         required: true
     },
-    posts: {
+    notes: {
         type: Array
     },
     projects: {
@@ -31,21 +29,65 @@ const userSchema = new mongoose.Schema({
     }
 });
 const User = mongoose.model("User", userSchema);
+const cat = {
+    title: "sample category",
+    desc: "Example of a category using Mongoose",
+    createTime: new Date(),
+    color: "black",
+    posts: [{
+        title: "Sample Note Title",
+        content: "Sample Note Content",
+        dueDate: "Sample Due Date"
+    }]
+}
+const Admin = new User({
+    email: "timothytuen45@gmail.com",
+    password: "password",
+    notes: [cat],
+    projects: []
+});
+// Admin.save();
 
-let signedIn = false;
-const categories = [];
 
-if(signedIn) {
+// User.find({email: "timothytuen45@gmail.com"}).posts
+User.find().then(function(users) {
+    users.forEach(function(user) {
+        console.log(user);
+        console.log("space");
+    })
+}).catch(function(err) {
+    console.log(err);
+});
+
+let signedIn = true;
+
     app.get("/", function(req,res) {
-        res.render("home");
+        if(signedIn) {
+            res.render("home");
+        } else {
+            res.redirect("/sign-in");
+        }
     })
 
     app.get("/list", function(req, res) {
-        res.render("list", {categories: categories});
+        if(signedIn) {
+            User.findOne({email: "timothytuen45@gmail.com"}).then(function(postCategories) {
+                let categoriesArr = postCategories.notes;
+                res.render("list", {categories: categoriesArr});
+            }).catch(function(err) {
+                console.log(err);
+            });
+        } else {
+            res.redirect("/sign-in")
+        }
     }) 
     
     app.get("/compose-category", function(req, res) {
-        res.render("composeCategory");
+        if(signedIn) {
+            res.render("composeCategory");
+        } else {
+            res.redirect("/sign-in")
+        }
     })
     
     app.post("/compose-category", function (req, res) {
@@ -53,7 +95,6 @@ if(signedIn) {
         const desc = req.body.catDesc;
         const color = req.body.catColor;
         const createTime = new Date();
-    
         const cat = {
             title: title,
             desc: desc,
@@ -61,44 +102,81 @@ if(signedIn) {
             color: color,
             posts: []
         }
-        // categories.push(cat);
-        User.updateMany({email: req.user.email}, {$push: {posts: cat}}) //Push the new category to the posts array in the user's database entry
-        res.redirect("/task/" + title);
+        User.findOne({email: "timothytuen45@gmail.com"}).then(function(user) {
+            user.notes.push(cat);
+            user.save().then(() => {
+                console.log(user.notes);
+                res.redirect("/task/" + title);
+            });
+        }).catch(function(err) {
+            console.log(err);
+        })
     })
     
     app.get("/task/:name", function(req, res) {
-        // const result = categories.findIndex(item => item.title === _.lowerCase(req.params.name));
-        const result = User.find({email: req.user.email}, 'posts').findIndex(item => item.title === _.lowerCase(req.params.name));
-        res.render("categoryTasks", {category: categories[result]});
+        console.log("get task");
+        if(signedIn) {
+            User.findOne({email: "timothytuen45@gmail.com"}).then(function(userData) {
+                const categoryIndex = userData.notes.findIndex(item => item.title === _.lowerCase(req.params.name));
+                res.render("categoryTasks", {category: userData.notes[categoryIndex]});
+            }).catch(function(err) {
+                console.log(err);
+            });
+        } else {
+            res.redirect("/sign-in")
+        }
+
     });
     
     app.post("/task/:name", function(req, res) {
-        const result = categories.findIndex(item => item.title === _.lowerCase(req.params.name));
-        const post = {
-            title: req.body.postTitle,
-            content: req.body.postInfo,
-            dueDate: req.body.postDate
-        }
-        categories[result].posts.push(post);
-        res.redirect("/task/" + req.params.name);
+        User.findOne({email: "timothytuen45@gmail.com"}).then(function(user) {
+            const post = {
+                title: req.body.postTitle,
+                content: req.body.postInfo,
+                dueDate: req.body.postDate
+            }
+            const categoryIndex = user.notes.findIndex(item => item.title === _.lowerCase(req.params.name));
+            user.notes[categoryIndex].posts.push(post);
+            user.markModified('notes'); //Previously not saving because didn't know it was modified due to posts being so deep into the object. Tell mongoDB there's a change here
+            user.save().then(() => {
+                console.log("SAVED");
+                res.redirect("/task/" + req.params.name)
+            });
+            return;
+        }).catch(function(err) {
+            console.log(err);
+        });
     });
     
     app.post("/task/:name/delete", function(req, res) {
     
         const task = req.body.deleteTask;
-        const index = categories.findIndex(item => item.title === _.lowerCase(req.params.name));
-        const postIndex = categories[index].posts.findIndex(item => item.title === task);
-        categories[index].posts.splice(postIndex, 1);
-        res.redirect("/task/" + req.params.name);
+        User.findOne({email: "timothytuen45@gmail.com"}).then(function(user) {
+            const categoryIndex = user.notes.findIndex(item => item.title === _.lowerCase(req.params.name));
+            const postIndex = user.notes[categoryIndex].posts.findIndex(item => item.title === task);
+            user.notes[categoryIndex].posts.splice(postIndex, 1);
+            user.markModified('notes');
+            user.save().then(() => {
+                res.redirect("/task/" + req.params.name);
+            })
+        }).catch(function(err) {
+            console.log(err);
+        })
     })
     
     app.post("/list/delete", function(req, res) {
         const cat = req.body.deleteCategory;
-        const index = categories.findIndex(item => item.title === cat);
-        categories.splice(index, 1);
-        res.redirect("/list");
+        User.findOne({email: "timothytuen45@gmail.com"}).then(function(user) {
+            const categoryIndex = user.notes.findIndex(item => item.title === cat);
+            user.notes.splice(categoryIndex, 1);
+            user.save().then(() => {
+                res.redirect("/list");
+            })
+        }).catch(function(err) {
+            console.log(err);
+        })
     })
-} else {
+
 
     app.get("/sign-in", function(req, res) {
         res.render("signIn");
@@ -118,14 +196,14 @@ if(signedIn) {
         const passwordCreated = req.body.password;
         const passwordCheck = req.body.passwordTwo;
         const emailCreated = req.body.email;
-        if(passwordCreated != passwordCheck) {
-            alert("Password fields do not match");
-            validAccount = false;
-        } 
-        if(emailCreated is taken){
-            alert("Email has already been registered");
-            validAccount = false;
-        }
+        // if(passwordCreated != passwordCheck) {
+        //     alert("Password fields do not match");
+        //     validAccount = false;
+        // } 
+        // if(emailCreated is taken){
+        //     alert("Email has already been registered");
+        //     validAccount = false;
+        // }
 
         if(validAccount) {
             signedIn = true;
@@ -139,7 +217,7 @@ if(signedIn) {
             // res.redirect("/");
         }
     })
-}
+
 
 
 
