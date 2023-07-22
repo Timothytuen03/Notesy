1094b4917297315792dd341d53b3a32612da8a20
Timothy Tuen <timothytuen45@gmail.com>
2023-07-22 13:16:20 -0400

    Styling, fixed errors with post requests

--- app.js
+++ app.js
@@ -60,14 +60,6 @@ passport.deserializeUser(User.deserializeUser());
 
 
 
-// User.find().then(function(users) {
-//     users.forEach(function(user) {
-//         console.log(user);
-//         console.log("space");
-//     })
-// }).catch(function(err) {
-//     console.log(err);
-// });
 
 let currUserId;
 
@@ -88,10 +80,7 @@ app.get("/home", connectEnsureLogin.ensureLoggedIn(), function(req, res) {
 })
 
 app.get("/list", connectEnsureLogin.ensureLoggedIn(), function(req, res) {
-    console.log("notes & logged in");
     User.findOne({username: req.user.username}).then(function(postCategories) {
-        console.log(postCategories);
-        console.log(postCategories.notes);
         let categoriesArr = postCategories.notes;
         res.render("list", {categories: categoriesArr});
     }).catch(function(err) {
@@ -118,7 +107,6 @@ app.post("/compose-category", function (req, res) {
     User.findOne({username: req.user.username}).then(function(user) {
         user.notes.push(cat);
         user.save().then(() => {
-            console.log(user.notes);
             res.redirect("/task/" + title);
         });
     }).catch(function(err) {
@@ -147,7 +135,6 @@ app.post("/task/:name", function(req, res) {
         user.notes[categoryIndex].posts.push(post);
         user.markModified('notes'); //Previously not saving because didn't know it was modified due to posts being so deep into the object. Tell mongoDB there's a change here
         user.save().then(() => {
-            console.log("SAVED");
             res.redirect("/task/" + req.params.name)
         });
         return;
@@ -183,7 +170,8 @@ app.post("/task/:name/complete", function(req, res) {
             categoryName: category,
             postTitle: post.title,
             postContent: post.content,
-            postDue: post.dueDate
+            postDue: post.dueDate,
+            dateComplete: new Date()
         })
         user.notes[categoryIndex].posts.splice(postIndex, 1);
         user.markModified('notes');
@@ -228,7 +216,9 @@ app.post("/compose-project", function(req, res) {
             projectTitle: _.lowerCase(req.body.projectTitle),
             projectDescription: req.body.projectDescription,
             techStack: req.body.techStack,
-            progress: req.body.progress
+            progress: req.body.progress,
+            dateCreated: new Date(),
+            dateComplete: null
         }
         if(newProject.progress === "Completed") {
             user.completedProjects.push(newProject);
@@ -259,25 +249,53 @@ app.post("/projects/delete", function(req, res) {
 })
 
 app.get("/edit-project/:projectName", connectEnsureLogin.ensureLoggedIn(), function(req, res) {
-    console.log(req.params.projectName);
     User.findOne({username: req.user.username}).then((user)=> {
-        const projectIndex = user.projects.findIndex(item => item.projectTitle === req.params.projectName);
-        res.render("editProject", {project: user.projects[projectIndex]});
+        let projectIndex = user.projects.findIndex(item => item.projectTitle === req.params.projectName);
+        if(projectIndex === -1) {
+            projectIndex = user.completedProjects.findIndex(item => item.projectTitle === req.params.projectName);
+            res.render("editProject", {project: user.completedProjects[projectIndex]});
+        } else {
+            res.render("editProject", {project: user.projects[projectIndex]});
+        }
     })
 });
 
 app.post("/edit-project/:projectName", function(req, res) {
     User.findOne({username: req.user.username}).then((user)=> {
-        const projectIndex = user.projects.findIndex(item => item.projectTitle === req.params.projectName);
-        user.projects[projectIndex].projectTitle = req.body.projectTitle;
-        user.projects[projectIndex].projectDescription = req.body.projectDescription;
-        user.projects[projectIndex].techStack = req.body.techStack;
-        user.projects[projectIndex].progress = req.body.progress;
-        user.markModified('projects');
-        if(req.body.progress === "Completed") {
+        let projectIndex = user.projects.findIndex(item => item.projectTitle === req.params.projectName);
+        console.log(projectIndex);
+        let completePrior = false;
+        if(projectIndex === -1) {
+            projectIndex = user.completedProjects.findIndex(item => item.projectTitle === req.params.projectName);
+            completePrior = true;
+        }
+        const postProgress = req.body.progress;
+        if(!completePrior) {
+            user.projects[projectIndex].projectTitle = req.body.projectTitle;
+            user.projects[projectIndex].projectDescription = req.body.projectDescription;
+            user.projects[projectIndex].techStack = req.body.techStack;
+            user.projects[projectIndex].progress = req.body.progress;
+            user.markModified('projects');
+        } else {
+            // console.log(projectIndex + " " + user.completedProjects + " " + req.body.projectTitle);
+            user.completedProjects[projectIndex].projectTitle = req.body.projectTitle;
+            user.completedProjects[projectIndex].projectDescription = req.body.projectDescription;
+            user.completedProjects[projectIndex].techStack = req.body.techStack;
+            user.completedProjects[projectIndex].progress = req.body.progress;
+            user.markModified('completedProjects');
+        }
+        if(!completePrior && postProgress === "Completed") {
+            // Not complete before, complete now
+            user.projects[projectIndex].dateComplete = new Date();
             const completedProj = user.projects[projectIndex];
             user.projects.splice(projectIndex, 1);
             user.completedProjects.push(completedProj);
+        }else if(completePrior && postProgress !== "Completed") {
+            // Complete before, now not
+            user.completedProjects[projectIndex].dateComplete = null;
+            const completedProj = user.completedProjects[projectIndex];
+            user.completedProjects.splice(projectIndex, 1);
+            user.projects.push(completedProj);
         }
         user.save().then(()=> {
             res.redirect("/projects");
@@ -293,6 +311,16 @@ app.get("/completed", connectEnsureLogin.ensureLoggedIn(), function(req, res) {
     })
 })
 
+app.post("/completed/delete-task", function(req, res) {
+    User.findOne({_id: req.user._id}).then((user)=> {
+        const completeTaskIndex = user.completedNotes.findIndex(index => index.postTitle === req.body.deleteCompleteTask);
+        user.completedNotes.splice(completeTaskIndex, 1);
+        user.save().then(() => {
+            res.redirect("/completed");
+        });
+    })
+})
+
 
 
 
@@ -304,7 +332,6 @@ app.get("/login", function(req, res) {
 app.post("/login", passport.authenticate('local', {failureRedirect: '/login'}), function(req, res) {
     // const usernameSubmitted = req.body.username;
     // const passwordSubmitted = req.body.password;
-    console.log(req.user)
     res.redirect("/home");
 })
 
@@ -313,28 +340,29 @@ app.get("/create-account", function(req, res) {
 })
 
 app.post("/create-account",  function(req, res) {
-    const newUserEntry = new User({
-        username: req.body.username,
-        password: req.body.password,
-        notes: [],
-        projects: []
-    });
-
-    User.register(newUserEntry, req.body.password, function(err, user) {
-        if(err){
-            console.log(err);
-            res.redirect("/create-account");
-        } else {
-            console.log(user + "2");
-            passport.authenticate("local")(req, res, function() {
-                res.redirect("/home")
-            })
-        }
-    })
-
-    // newUserEntry.save().then(() => {
-    //     res.redirect("/home");
-    // })
+    if(req.body.password !== req.body.confirmPassword) {
+        console.log("Passwords do not match!");
+        return;
+        // res.redirect("/create-account");
+    } else {
+        const newUserEntry = new User({
+            username: req.body.username,
+            password: req.body.password,
+            notes: [],
+            projects: []
+        });
+    
+        User.register(newUserEntry, req.body.password, function(err, user) {
+            if(err){
+                console.log(err);
+                res.redirect("/create-account");
+            } else {
+                passport.authenticate("local")(req, res, function() {
+                    res.redirect("/home")
+                })
+            }
+        })
+    }
 })
 
 app.get("/logout", function(req, res) {
